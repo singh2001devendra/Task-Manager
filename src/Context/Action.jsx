873ce58eb10e71e9af2c84eff58324b213c9eced
Devendra singh thakur <129576872+singh2001devendra@users.noreply.github.com>
2024-11-13 20:17:39 +0530

    Update Action.jsx

--- src/Context/Action.jsx
+++ src/Context/Action.jsx
@@ -15,7 +15,7 @@ const Action = (props) => {
         className="btn btn-secondary"
         onClick={() =>
           props.handleChangePriority(
-            prompt("Enter new priority:", props.selectedTask.priority)
+            prompt("Enter new priority:", props.selectedTask.pr)
           )
         }
       >
